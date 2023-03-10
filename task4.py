51e0a8f1eb0e664270a977dc34c20d3ec33c4423
TatianaSavchenko <tania95044519@mail.ru>
2023-03-10 17:17:23 -0300

    Solved Task4

---
+++ task4.py
@@ -0,0 +1,8 @@
+# Найдите сумму цифр трехзначного числа.
+# *Пример:*
+# 123 -> 6 (1 + 2 + 3)
+# 100 -> 1 (1 + 0 + 0) |
+
+number = int(input("Enter a three-digit number: "))
+sum = (number%10) +  (number//100) + (number//10%10)
+print(sum)
